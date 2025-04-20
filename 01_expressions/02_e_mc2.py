1558c19647a2a17bcc90926e3bc40368350c74ca
harisaltafb105 <haris_memon1@yahoo.com>
2025-04-21 00:47:00 +0500

    first commit

--- 01_expressions/02_e_mc2.py
+++ 01_expressions/02_e_mc2.py
@@ -3,17 +3,16 @@ C: int = 299792458
 def main():
     mass_in_kg: float = float(input("Enter kilos of mass: "))
 
-    # Calculate energy
-    # equivalently energy = mass * (C ** 2)
-    # using the ** operator to raise C to the power of 2
+    # Calculate energy using E = mc^2
     energy_in_joules: float = mass_in_kg * (C ** 2)
 
-    # Display work to the user
+    # Print the result
+    print("Energy in joules:")
     print("e = m * C^2...")
     print("m = " + str(mass_in_kg) + " kg")
     print("C = " + str(C) + " m/s")
     
-    print(str(energy_in_joules) + " joules of energy!")
+    print("e = " + str(energy_in_joules) + " J")
 
 if __name__ == '__main__':
     main()
